c48bb81615bb4e2d2835c88d525366017c1edb8d
Svetlana <spushkova@mail.ru>
2020-10-07 21:34:45 +0300

    Класс Point

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -11,5 +11,12 @@ namespace Snake
         public int x;
         public int y;
         public char sym;
-    }
+
+        public void Draw()
+        {   
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+}
+
 }
